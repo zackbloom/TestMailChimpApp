310aeb451f89bd56587481add00515b75ee327da
Zack Bloom <zackbloom@gmail.com>
2015-09-17 18:24:59 -0400

    Fix url

--- helper.js
+++ helper.js
@@ -28,6 +28,7 @@
     }
 
     var url = INSTALL_OPTIONS.list;
+    url = url.replace('http', 'https');
     url = url.replace('?', '/post-json?');
     url = url + "&EMAIL=" + encodeURIComponent(input.value);
     url = url + "&c=" + cb;
